c0fe89da7a0463ee135f8bcbf0c2a61543a33b77
zergqw <eremenko.denis-qw@yandex.ru>
2025-03-03 20:04:40 +0300

    update3

--- src/init.js
+++ src/init.js
@@ -2,18 +2,22 @@ import i18next from 'i18next';
 import 'bootstrap'
 import * as yup from 'yup'
 
+import axios from 'axios';
+import _ from 'lodash';
+
 import initView from './watchers.js';
 import resources from './locales/index.js';
 import locale from './locales/locale.js';
+import parse from './rss.js';
 
 export default () => {
     const elements = {
-        form: document.querySelector('form'),
-        input: document.querySelector('#url-input'),
-        button: document.querySelector('button[type="submit"]'),
-        feedbackContainer: document.querySelector('.feedback'),
-        postsContainer: document.querySelector('.posts'),
-        feedsContainer: document.querySelector('.feeds'),
+        form: document.querySelector('.fss-form'),
+        input: document.querySelector('.fss-form input'),
+        feedback: document.querySelector('.feedback'),
+        submit: document.querySelector('.fss-form button[type="submit"]'),
+        postBox: document.querySelector('.posts'),
+        feedsBox: document.querySelector('.feeds'),
         modal: {
           title: document.querySelector('.modal-title'),
           body: document.querySelector('.modal-body'),
@@ -22,8 +26,16 @@ export default () => {
         spanSpinner: document.createElement('span'),
         spanLoading: document.createElement('span'),
       };
-    
-      const initialState = {
+    const getLoadingProcessErrorType = (e) => {
+        if (e.isParsingError) {
+            return 'noRss'
+        }
+        if (e.isAxionsError) {
+            return 'network'
+        }
+        return 'unknow'
+    }
+    const initialState = {
         rssForm: {
           state: 'filling',
           error: null,
@@ -31,14 +43,45 @@ export default () => {
         },
         feeds: [],
         posts: [],
-        uiState: {
-          visitedPosts: new Set(),
-          modalId: null,
+        loadingProcess: {
+          status: 'idle',
+          error: null,
         },
       };
-    
+    const addProxy = (url) => {
+        const urlWithProxy = new URL('/get', 'https://allorigins.hexlet.app')
+        urlWithProxy.searchParams.set('url', url)
+        return urlWithProxy.toString()
+
+    }
+    const loadRss = (watchedState, url) => {
+        watchedState.loadingProcess.status = 'loading';
+        const urlWithProxy = addProxy(url);
+        return axios.get(urlWithProxy)
+            .then((response) => {
+                    const data = parse(response.data.contents);
+                    const feed = {
+                        url, id: _.uniqueId(), title: data.title, description: data.description,
+                    };
+                    const posts = data.items.map((item) => ({ ...item, channelId: feed.id, id: _.uniqueId() }))
+                    watchedState.posts.unshift(...posts);
+                    watchedState.feeds.unshift(feed);
+
+                    watchedState.loadingProcess.error = null;
+                    watchedState.loadingProcess.status = 'idle';
+                    watchedState.form = {
+                        ...watchedState.form,
+                        status: 'filling',
+                        error: null,
+                    };
+        })
+        .catch((e) => {
+            watchedState.loadingProcess.error = getLoadingProcessErrorType(e);
+            watchedState.loadingProcess.status = 'failed';
+        });
+    }
       const i18n = i18next.createInstance();
-      return i18n.init({
+      const promise = i18n.init({
         lng: 'ru',
         resources,
       })
@@ -68,6 +111,7 @@ export default () => {
                             error: null,
                             valid: true,
                         }
+                        loadRss(watchedState, url)
                     } else {
                         watchedState.form = {
                             ...watchedState.form,
@@ -79,4 +123,5 @@ export default () => {
             )
         })
     })
+    return promise
 }
--- src/watchers.js
+++ src/watchers.js
@@ -13,16 +13,130 @@ export default (initState, elements, i18next) => {
             feedback.textContent = i18next.t(`errors.${error}`);
         }
     }
+    const handleFeeds= () => {
+        const { feeds } = initState;
+        const { feedsBox } = elements;
+        
+        const fragmentStructure = document.createElement('div')
+        fragmentStructure .classList.add('card', 'border-0')
+        fragmentStructure.innerHTML = `<div class ='card-body'></div>`
 
-    const watchedState = onChange(initState, (path) => {
-        switch (path) {
-            case 'form':
-                handleForm();
-                break;
+        const feedsTitle = document.createElement('h2');
+        feedsTitle.classList.add('card-title', 'h4');
+        feedsTitle.textContent = i18next.t('feeds');
+        fragmentStructure.querySelector('.card-body').appendChild(feedsTitle);
+
+        const feedsList = document.createElement('ul');
+        feedsList.classList.add('list-group', 'border-0', 'rounded-0');
+
+        const feedsListItems = feeds.map((feed) => {
+            const element = document.createElement('li');
+            element.classList.add('list-group-item', 'border-0', 'border-end-0');
+            const title = document.createElement('h3');
+            title.classList.add('h6', 'm-0');
+            title.textContent = feed.title;
+            const description = document.createElement('p');
+            description.classList.add('m-0', 'small', 'text-black-50');
+            description.textContent = feed.description;
+            element.append(title, description);
+            return element;
+        });
+        feedsList.append(...feedsListItems);
+        fragmentStructure.appendChild(feedsList);
+        feedsBox.innerHTML = ''
+        feedsBox.appendChild(fragmentStructure);
+    }
+    const handlePosts = () => {
+        const { posts } = initState;
+        const { postbox } = elements;
+
+        const fragmentStructure = document.createElement('div');
+        fragmentStructure.classList.add('card', 'border-0');
+        fragmentStructure.innerHTML = `<div class='card-body'></div>`
+
+        const postsTitle = document.createElement('h2');
+        postsTitle.classList.add('card-title', 'h4');
+        postsTitle.textContent = i18next.t('posts');
+        fragmentStructure.querySelector('.card-body').appendChild(postsTitle);
+
+        const postsList = document.createElement('ul');
+        postsList.classList.add('list-group', 'border-0', 'rounded-0');
+        
+        const postsListItems = posts.map((post) => {
+            const element = document.createElement('li');
+            element.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start','border-0', 'border-end')
+            const link = document.createElement('a');
+            link.setAttribute('href', post.link);
+            link.classList.add('fw-bold');
+            link.dataset.id = post.id;
+            link.textContent = post.title;
+            link.setAttribute('target', '_blank');
+            link.setAttribute('rel', 'noopener noreferrer');
+            element.appendChild(link);
+            const button = document.createElement('button');
+            button.setAttribute('type', 'button');
+            button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+            button.dataset.id = post.id;
+            button.dataset.bsToggle = 'modal';
+            button.dataset.bsTarget = '#modal';
+            button.textContent = i18next.t('preview');
+            element.appendChild(button);
+            return element;
+            });
+        postsList.append(...postsListItems)
+        fragmentStructure.appendChild(postsList)
+        postbox.innerHTML = ''
+        postbox.appendChild(fragmentStructure)
+        }     
+    const handleLoadingProcess = () => {
+        const {LoadingProcess} = initState
+        const {submit, input, feedback} = elements
+        switch (LoadingProcess.status) {
+            case 'failed':
+                submit.disable = false
+                input.removeAttribute('readonly')
+                feedback.classList.add('text-danger')
+                feedback.textContent = i18next.t([`errors.${LoadingProcess.error}`, 'errors.oknown'])
+                break
+            case 'idle':
+                submit.disable = false
+                input.removeAttribute('readonly')
+                input.value = ''
+                feedback.classList.add('text-success')
+                feedback.textContent = i18next.t('loading.success')
+                input.focus()
+                break  
+            case 'loading':
+                submit.disable = true
+                input.removeAttribute('readonly', true)
+                feedback.classList.remove('text-success')
+                feedback.classList.remove('text-danger')
+                feedback.textContent = ''
+                break 
             default:
-                break;
+                throw new Error(`'Unknown loadingProcess status: '${LoadingProcess.status}`)
+
         }
-    })
+    }
+        const watchedState = onChange(initState, (path) => {
+            switch (path) {
+                case 'form':
+                    handleForm();
+                    break;
+                case 'feeds':
+                    handleFeeds()
+                    break
+                case 'loadingProcess.status':
+                    handleLoadingProcess()
+                    break
+                case 'posts':
+                    handlePosts()
+                    break
+                default:
+                    break;
+            }
+        })
+    
 
     return watchedState;
 }
